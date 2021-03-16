fa02bb146bf144355c2a168d80ef667935a0b6a8
UncleLoony <sstas2003@gmail.com>
2021-03-16 18:00:52 +0600

    Обновление

--- src/components/Auth/LoginForm/LoginFormStyles.js
+++ src/components/Auth/LoginForm/LoginFormStyles.js
@@ -2,11 +2,13 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export const useStyles = makeStyles(theme => ({
     loginContainer:{
-        margin:"auto",
-        flex:"none",
-        padding:0,
-        width:"auto",
-        backgroundColor:"inherit",
+        '.MuiGrid-root&':{
+            margin:"auto",
+            flex:"none",
+            padding:0,
+            width:"auto",
+            backgroundColor:"inherit",
+        }
     },
     loginLogo:{
         textAlign:"center",
